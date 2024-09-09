5521d50aafeb3bc064a0391aa48c7644d5fb414c
Yogesh4635 <138120625+Yogesh4635@users.noreply.github.com>
2024-09-09 18:12:58 +0000

    comparion and equal

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+//  console.log(2 > 1);
+//  console.log(2 < 1);
+//  console.log(2 >= 1);
+//  console.log(2 == 1);
+//  console.log(2 != 1);
+
+//  console.log("2" > 1);      predictable results are not their.
+//  console.log("02" > 1);
+
+console.log(null > 0);          //   === It is called strictly check in which it will check the data of the variable by which we are comparing.
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+
+
+
+
+
+
+
