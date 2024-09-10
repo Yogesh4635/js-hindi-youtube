2a127d4ed4e6469904c07592e135b264ebcb0c99
Yogesh4635 <138120625+Yogesh4635@users.noreply.github.com>
2024-09-10 19:57:33 +0000

    String brief

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+const name = "Yogesh"
+const repoCount = 50
+
+//  console.log(name + repoCount + "Value");
+
+console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+
+const gameName = new String('yogesh-hc-ut-po')
+
+console.log(gameName[2]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('h'));
+console.log(gameName.substring(0,4));
+console.log(gameName.slice(-5,2));
+const newStringOne = "   happy   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://yogesh.com/hitesh%20chaudhary"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
+
